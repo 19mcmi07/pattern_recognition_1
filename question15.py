35822472bb499158a086f58d2fcf79988667da75
Rohan Yadav <19mcmi07@gmail.com>
2019-10-06 12:32:04 -0700

    refinement to ques 15 and 16

--- question15.py
+++ question15.py
@@ -1,32 +1,49 @@
-import pylab as pl
 import numpy as np
+import matplotlib.pylab as plt
 
-D = 2
 
-M1 = np.array([0.0, 0.0])
-M2 = np.array([1.0, 1.0])
+def generate_random_x(dimension):
+    current_x = []
 
-C1 = np.array([[2.0, 0.4], [0.4, 1.0]])
-C2 = np.array([[1.0, 0.6], [0.6, 2.0]])
+    for i in range(dimension):
+        g = round(np.random.random() * 10)
+        current_x.append(g)
 
-X, Y = np.mgrid[-2:2:100j, -2:2:100j]
-points = np.c_[X.ravel(), Y.ravel()]
+    return current_x
 
-invC = np.linalg.inv(C1)
-v = points - M1
-g1 = -0.5*np.sum(np.dot(v, invC) * v, axis=1) - D*0.5*np.log(2*np.pi) - 0.5*np.log(np.linalg.det(C1))
-g1.shape = 100, 100
 
-invC = np.linalg.inv(C2)
-v = points - M2
-g2 = -0.5*np.sum(np.dot(v, invC) * v, axis=1) - D*0.5*np.log(2*np.pi) - 0.5*np.log(np.linalg.det(C2))
-g2.shape = 100, 100
+def main():
+    print("Question 15")
+    dimension = 2
+    mean1 = np.array([0.0, 0.0])
+    mean2 = np.array([1.0, 1.0])
+    prior1 = 0.5
+    prior2 = 0.5
+    covariance1 = np.array([[2.0, 0.4], [0.4, 1.0]])
+    covariance2 = np.array([[1.0, 0.6], [0.6, 2.0]])
 
-fig, axes = pl.subplots(1, 3, figsize=(15, 5))
-ax1, ax2, ax3 = axes.ravel()
-for ax in axes.ravel():
-    ax.set_aspect("equal")
+    result1 = []
+    result2 = []
+    for i in range(1000):
+        points = np.array(generate_random_x(dimension))
+        
+        inv_of_cov1 = np.linalg.inv(covariance1)
+        x_mean1 = points - mean1
+        g1 = -0.5 * np.sum(np.dot(x_mean1, inv_of_cov1) * x_mean1) - dimension * 0.5 * np.log(2 * np.pi) - 0.5 * \
+            np.log(np.linalg.det(covariance1)) + np.log(prior1)
+        
+        result1.append(g1)
+        
+        inv_of_cov2 = np.linalg.inv(covariance2)
+        x_mean2 = points - mean2
+        g2 = -0.5 * np.sum(np.dot(x_mean2, inv_of_cov2) * x_mean2) - dimension * 0.5 * np.log(2 * np.pi) - 0.5 * \
+            np.log(np.linalg.det(covariance2)) + prior2
+        result2.append(g2)
 
-ax1.pcolormesh(X, Y, g1)
-ax2.pcolormesh(X, Y, g2)
-ax3.pcolormesh(X, Y, g1 > g2)
+    plt.plot(result1)
+    plt.plot(result2)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- question16.py
+++ question16.py
@@ -5,8 +5,18 @@ def main():
     print("Question 16")
     mean1, mean2, variance1, variance2 = 0, 5, 1, 1
     x1, x2 = [], []
-    for i in range(5):
-        t = (1. / (2*np.pi))
+    num = int(input("Enter number of sample"))
+    for i in range(num):
+        x = np.random.uniform()
+        t1 = (1. / np.sqrt(2*np.pi)) * (np.exp(-((x-mean1)**2) * 0.5))
+        t2 = (1. / np.sqrt(2*np.pi)) * (np.exp(-((x-mean2)**2) * 0.5))
+        x1.append(t1)
+        x2.append(t2)
+    
+    print(np.mean(x1))
+    print(np.var(x1))
+    print(np.mean(x2))
+    print(np.var(x2))
 
 
 if __name__ == "__main__":
